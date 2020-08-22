781689db535ebce61f2b8452c3d3ec7b15514fc1
xtompie <tomasz.pielczyk@gmail.com>
2020-08-22 13:44:35 +0200

    no mix in view

--- resources/views/admin/layout/default.blade.php
+++ resources/views/admin/layout/default.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ mix('resources/admin/main.js') }}" defer></script>
+    <script src="resources/admin/js/app.js"></script>
 
 
     <!-- Styles -->
